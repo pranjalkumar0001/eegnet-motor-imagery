e9d7a782d51cdb2bb9cb3bfc525c1b195557ae2d
pranjalkumar0001 <pranjalkumar.pk.0001@gmail.com>
2026-05-20 14:37:46 +0530

    first neuron implementation from scratch

---
+++ neuron.py
@@ -0,0 +1,16 @@
+import math
+
+def sigmoid(x):
+    return 1/(1+math.exp(-x))
+
+def neuron(weights, inputs, bias):
+    z = sum((w * i for w,i in zip(weights, inputs)))
+    z += bias
+    return sigmoid(z)
+
+#test
+inputs = [1.0, 2.0, 3.0]
+weights = [0,0,0]
+bias = 0.0
+output = neuron(weights, inputs, bias)
+print(f"output = {output}")

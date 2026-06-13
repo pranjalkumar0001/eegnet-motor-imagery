6bcc2c58221379e7efdab4b5f9c0580fc435b01f
pranjalkumar0001 <pranjalkumar.pk.0001@gmail.com>
2026-06-13 15:36:05 +0530

    saving epoches for further reference

--- MNE-python_projects/01_physionet_ingest.py
+++ MNE-python_projects/01_physionet_ingest.py
@@ -24,3 +24,4 @@ print(event_id_dict) #T00: rest, T1: left hand, T2: right hand
 custom_mapping = {'rest': 1, 'left_fist': 2, 'right_fist': 3}
 epochs = mne.Epochs(raw,events, event_id=custom_mapping, tmin= -1, tmax= 4, preload=True)
 print(epochs)
+epochs.save("subject1-epo.fif", overwrite=True)

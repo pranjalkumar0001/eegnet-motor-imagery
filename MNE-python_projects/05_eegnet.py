6ba63e29797f4247cd01b71363bcce4f44dd0a33
pranjalkumar0001 <pranjalkumar.pk.0001@gmail.com>
2026-06-22 09:12:44 +0530

    implemented EEGNet architecture and verify forward pass tensor shapes

---
+++ MNE-python_projects/05_eegnet.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+X_raw = np.load("X_binary.npy")
+y_raw = np.load("y_binary.npy") -2
+
+X = torch.tensor(X_raw, dtype=torch.float32)
+y = torch.tensor(y_raw, dtype= torch.long)
+
+X = X.unsqueeze(1) # X.size is (45,1,64,801)
+
+class EEGNet(nn.Module):
+    def __init__(self, channels = 64, samples = 801, classes = 2):
+        super(EEGNet, self).__init__()
+
+        # Block 1: Temporal Convolution (Acts like the Bandpass Filter)
+        self.conv1 = nn.Conv2d(1,8, (1,64), padding='same', bias=False)
+        self.batchnorm1 = nn.BatchNorm2d(8)
+
+        # Block 2: Depthwise Spatial Convolution (Acts like CSP)
+        self.depthwise = nn.Conv2d(8, 16, (channels, 1), groups=8, bias=False)
+        self.batchnorm2 = nn.BatchNorm2d(16)
+        self.elu1 = nn.ELU()
+        self.pool1 = nn.AvgPool2d((1, 4))
+        self.dropout1 = nn.Dropout(0.25)
+
+        # Block 3: Separable Convolution (Feature extraction)
+        self.separable = nn.Conv2d(16, 16, (1, 16), padding='same', groups=16, bias=False)
+        self.pointwise = nn.Conv2d(16, 16, (1, 1), bias=False)
+        self.batchnorm3 = nn.BatchNorm2d(16)
+        self.elu2 = nn.ELU()
+        self.pool2 = nn.AvgPool2d((1, 8))
+        self.dropout2 = nn.Dropout(0.25)
+
+        #Classifier Setup
+        self.flatten = nn.Flatten()
+
+        # Dynamically calculate the flattened dimension size for the linear layer
+        dummy_input = torch.randn(1, 1, channels, samples)
+        with torch.no_grad():
+            x_dummy = self.pool2(self.elu2(self.batchnorm3(self.pointwise(self.separable(
+                      self.dropout1(self.pool1(self.elu1(self.batchnorm2(self.depthwise(
+                      self.batchnorm1(self.conv1(dummy_input))))))))))))
+            flattened_size = x_dummy.shape[1] * x_dummy.shape[2] * x_dummy.shape[3]
+            
+        self.classifier = nn.Linear(flattened_size, classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.batchnorm1(x)
+        
+        x = self.depthwise(x)
+        x = self.batchnorm2(x)
+        x = self.elu1(x)
+        x = self.pool1(x)
+        x = self.dropout1(x)
+        
+        x = self.separable(x)
+        x = self.pointwise(x)
+        x = self.batchnorm3(x)
+        x = self.elu2(x)
+        x = self.pool2(x)
+        x = self.dropout2(x)
+        
+        x = self.flatten(x)
+        x = self.classifier(x)
+        return x
+    
+
+model = EEGNet()
+output = model(X)
+print(f"Output Tensor Shape: {output.shape}")
+print(output)
